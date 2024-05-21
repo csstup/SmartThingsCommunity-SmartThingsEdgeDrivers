3d79e912cb250391478b590f759a7f0c9e33c2cc
Corey Stup <CoreyStup@gmail.com>
2024-05-21 00:55:39 -0400

    added fingerprints for some other jasco fans

--- drivers/SmartThings/zwave-fan/src/zwave-fan-3-speed/init.lua
+++ drivers/SmartThings/zwave-fan/src/zwave-fan-3-speed/init.lua
@@ -25,8 +25,12 @@ local fan_speed_helper = (require "zwave_fan_helpers")
 local FAN_3_SPEED_FINGERPRINTS = {
   {mfr = 0x001D, prod = 0x1001, model = 0x0334}, -- Leviton 3-Speed Fan Controller
   {mfr = 0x0063, prod = 0x4944, model = 0x3034}, -- GE In-Wall Smart Fan Control
-  {mfr = 0x0063, prod = 0x4944, model = 0x3131}, -- GE In-Wall Smart Fan Control
-  {mfr = 0x0039, prod = 0x4944, model = 0x3131}, -- Honeywell Z-Wave Plus In-Wall Fan Speed Control
+  {mfr = 0x0063, prod = 0x4944, model = 0x3131}, -- GE In-Wall Smart Fan Control 39358 / 14287 / ZW4002
+  {mfr = 0x0039, prod = 0x4944, model = 0x3131}, -- Honeywell Z-Wave Plus In-Wall Fan Speed Control  
+  {mfr = 0x0063, prod = 0x4944, model = 0x3138}, -- GE In-Wall Smart Fan Control 14314 / ZW4002
+  {mfr = 0x0063, prod = 0x4944, model = 0x3337}, -- GE In-Wall Smart Fan Control
+  {mfr = 0x0063, prod = 0x4944, model = 0x3338}, -- GE In-Wall Smart Fan Control 55259 / ZW4002
+
 }
 
 local function map_fan_3_speed_to_switch_level (speed)
